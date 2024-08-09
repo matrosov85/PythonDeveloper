ff2fef0e5c572c8e97944e3ae500fb81080be1dd
matrosov85 <113940810+matrosov85@users.noreply.github.com>
2024-08-09 23:21:04 +0300

    Add files via upload

--- Module5/module_5_3.py
+++ Module5/module_5_3.py
@@ -19,32 +19,41 @@ class House:
         return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
 
     def __eq__(self, other):
-        return self.number_of_floors == other.number_of_floors
+        if isinstance(other, House):
+            return self.number_of_floors == other.number_of_floors
 
     def __lt__(self, other):
-        return self.number_of_floors < other.number_of_floors
+        if isinstance(other, House):
+            return self.number_of_floors < other.number_of_floors
 
     def __le__(self, other):
-        return self.number_of_floors <= other.number_of_floors
+        if isinstance(other, House):
+            return self.number_of_floors <= other.number_of_floors
 
     def __gt__(self, other):
-        return self.number_of_floors > other.number_of_floors
+        if isinstance(other, House):
+            return self.number_of_floors > other.number_of_floors
 
     def __ge__(self, other):
-        return self.number_of_floors >= other.number_of_floors
+        if isinstance(other, House):
+            return self.number_of_floors >= other.number_of_floors
 
     def __ne__(self, other):
-        return self.number_of_floors != other.number_of_floors
+        if isinstance(other, House):
+            return self.number_of_floors != other.number_of_floors
 
     def __add__(self, value):
-        self.number_of_floors += value
-        return self
+        if isinstance(value, int):
+            self.number_of_floors += value
+            return self
 
     def __radd__(self, value):
-        return self.__add__(value)
+        if isinstance(value, int):
+            return self.__add__(value)
 
     def __iadd__(self, value):
-        return self.__add__(value)
+        if isinstance(value, int):
+            return self.__add__(value)
 
 
 h1 = House('ЖК Эльбрус', 10)
